1f558926c01190e990f336b0a8cf58468aac3bb4
Suba Shrestha <anything963@hotmail.com>
2021-10-27 21:13:04 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Shubha Shrestha on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
